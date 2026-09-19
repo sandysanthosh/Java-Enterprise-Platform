d44b6b3b66b92c26eec9e9aa9f58bb3d0aa0827e
Santhosh Kumar Karthikeyan <santhosh072018@gmail.com>
2026-09-19 23:57:41 +0530

    feat: add enterprise platform foundation

---
+++ src/main/java/com/santhosh/platform/EnterprisePlatformApplication.java
@@ -0,0 +1,11 @@
+package com.santhosh.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterprisePlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnterprisePlatformApplication.class, args);
+    }
+}
